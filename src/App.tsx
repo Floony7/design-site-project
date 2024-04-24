fa2f693418a5d168c2b2916a0a497ac24c11e7a1
Floony7 <fred.lunjevich@stretchsense.com>
2024-04-24 09:51:56 +0100

    Project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import { GlobalStyles } from "./styles/global";
+
+function App() {
+  return (
+    <div>
+      <h1>Hi from React</h1>
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/global.ts
@@ -0,0 +1,8 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+html {
+    // colors
+    --clr-primary: border-left: unset;;
+}
+`;
