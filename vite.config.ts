663b39e2494a72edf50bfd35cfdce4975442630c
Floony7 <fred.lunjevich@stretchsense.com>
2024-04-26 11:37:50 +0100

    Changed base to ./

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path';
+// import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' ? '/assets/' : '/',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-  build: {
-    rollupOptions: {
-      external: ['prop-types'],
-    },
-  },
+  base: "./"
+  // base: process.env.NODE_ENV === 'production' ? '/assets/' : '/',
+  // resolve: {
+  //   alias: {
+  //     '@': path.resolve(__dirname, 'src'),
+  //   },
+  // },
+  // build: {
+  //   rollupOptions: {
+  //     external: ['prop-types'],
+  //   },
+  // },
 })
